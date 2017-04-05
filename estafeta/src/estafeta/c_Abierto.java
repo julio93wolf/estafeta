6f23fb69b75a2fddd50b3475265469def7bc0d4f
julio93wolf <juliovr_93@hotmail.com>
2017-04-05 01:45:25 -0500

    Estafeta: Codificacion de A*

---
+++ estafeta/src/estafeta/c_Abierto.java
@@ -0,0 +1,18 @@
+package estafeta;
+
+public class c_Abierto {
+    
+    private String a_Nodo;
+    private String a_Padre;
+    private float a_Fq;
+    private float a_Gq;
+    
+    public c_Abierto(String p_Nodo,String p_Padre,float p_Fq,float p_Gq){
+        a_Nodo=p_Nodo;
+        a_Padre=p_Padre;
+        a_Fq=p_Fq;
+        a_Gq=p_Gq;
+    }
+    
+    
+}
